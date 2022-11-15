f8bf4298a42e4383a5a513851dad19488076643d
Raphael Marques <raphaeljcmarques@gmail.com>
2022-11-15 10:17:31 -0300

    chore: setting up project

---
+++ api/commitlint.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    parserPreset: 'conventional-changelog-conventionalcommits',
+    rules: {
+        'subject-empty': [2, 'never'],
+        'type-empty': [2, 'never'],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'build',
+                'ci',
+                'docs',
+                'wip-docs',
+                'feat',
+                'wip-feat',
+                'fix',
+                'wip-fix',
+                'perf',
+                'wip-perf',
+                'revert',
+                'refactor',
+                'wip-refactor',
+                'style',
+                'wip-style',
+                'test',
+                'wip-test',
+                'chore',
+                'wip-chore',
+            ],
+        ],
+    },
+};
