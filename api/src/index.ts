272a171aa217aeaeb1290af1197a55cb6310f816
Raphael Marques <raphaeljcmarques@gmail.com>
2022-11-15 15:15:18 -0300

    feat: category routes

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { router } from './router';
 
 const PORT = 3001;
 
@@ -9,6 +10,10 @@ mongoose
     console.log('Connected to MongoDB');
 
     const app = express();
+
+    // Middlewares
+    app.use(express.json());
+    app.use(router);
 
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
---
+++ api/src/router.ts
@@ -0,0 +1,46 @@
+import { Router } from 'express';
+import { createCategory } from './app/useCases/categories/createCategory';
+import { listCategories } from './app/useCases/categories/listCategories';
+
+export const router = Router();
+
+// List categories
+router.get('/categories', listCategories);
+
+// Create category
+router.post('/categories', createCategory);
+
+// List products
+router.get('/products', (req, res) => {
+  res.send('OK');
+});
+
+// Create products
+router.post('/products', (req, res) => {
+  res.send('OK');
+});
+
+// Get products by category
+router.get('/categories/:categoryId/products', (req, res) => {
+  res.send('OK');
+});
+
+// List orders
+router.get('/orders', (req, res) => {
+  res.send('OK');
+});
+
+// Create orders
+router.post('/orders', (req, res) => {
+  res.send('OK');
+});
+
+// Change order status
+router.patch('/orders/:orderId', (req, res) => {
+  res.send('OK');
+});
+
+// Delete/cancel order
+router.delete('/orders/:orderId', (req, res) => {
+  res.send('OK');
+});
