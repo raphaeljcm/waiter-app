0cf7978916f34cf1caafc7d5fd0433144a7ac1c8
Raphael Marques <raphaeljcmarques@gmail.com>
2022-11-15 16:21:30 -0300

    feat: create middleware to serve static files

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'node:path';
 
 import { router } from './router';
 
@@ -13,6 +14,10 @@ mongoose
     const app = express();
 
     // Middlewares
+    app.use(
+      '/uploads',
+      express.static(path.resolve(__dirname, '../', 'uploads')),
+    );
     app.use(express.json());
     app.use(router);
 
