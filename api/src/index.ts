1c86c3b1e915bc56d670f4f71670a929855018f5
Raphael Marques <raphaeljcmarques@gmail.com>
2022-11-15 12:06:01 -0300

    chore: setting up mongodb

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const PORT = 3001;
 
-const app = express();
+mongoose
+  .connect('mongodb://localhost:27017')
+  .then(() => {
+    console.log('Connected to MongoDB');
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+    const app = express();
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch(() => console.log('Failed to connect to MongoDB'));
